c1b42a66469abd8e46aa6b535251ba5920e8d58f
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-26 09:13:42 +0600

    push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const catagoryes = require('./data/catagory.json');
-
+const topic = require('./data/topic.json');
 
 app.get('/', (req, res) =>{
     res.send('java script is running');
@@ -15,6 +15,20 @@ app.get('/', (req, res) =>{
 app.get('/topic-catagory', (req, res) =>{
     res.send(catagoryes)
     
+})
+app.get('/catagory/:id', (req, res) =>{
+    const id = req.params.id
+    const selectedCatagory = topic.filter(sc => sc.category_id === id )
+    res.send(selectedCatagory)
+})
+app.get('/topic', (req, res) =>{
+    res.send(topic)
+})
+app.get('/topic/:id', (req, res) =>{
+    console.log(req.params)
+    const id = req.params.id;
+    const selectedTopic = topic.find(tp => tp._id === id)
+    res.send(selectedTopic)
 })
 
 app.listen(port, () =>{
