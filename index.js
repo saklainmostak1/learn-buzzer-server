cb0c281aa5e2935b0f1c28f98645db0902bd943d
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-27 01:03:22 +0600

    data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors())
 
 const catagoryes = require('./data/catagory.json');
 const topic = require('./data/topic.json');
+const premium = require('./data/premium.json')
 
 app.get('/', (req, res) =>{
     res.send('java script is running');
@@ -29,6 +30,16 @@ app.get('/topic/:id', (req, res) =>{
     const id = req.params.id;
     const selectedTopic = topic.find(tp => tp._id === id)
     res.send(selectedTopic)
+})
+
+app.get('/premium', (req, res) =>{
+    res.send(premium)
+})
+app.get('/premium/:id', (req, res) =>{
+    console.log(req.params)
+    const id = req.params.id;
+    const selectedPremium = premium.find(tp => tp.id === id)
+    res.send(selectedPremium)
 })
 
 app.listen(port, () =>{
